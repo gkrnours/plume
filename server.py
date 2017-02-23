219dcb33d06f68251a6082cee226d7029120f4f4
ludovic coues <gkr@jellycopter.net>
2017-02-23 22:19:00 +0100

    setup templates

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 #! /usr/bin/env python
 from flask import Flask
+
+from utils import templated
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "plume"
+@templated()
+def home():
+    pass
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", "5000")
+    app.run("0.0.0.0", 5000, debug=True)
