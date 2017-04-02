3e5c26af097ba3d528c414819594cb015e6b0241
ludovic coues <gkr@jellycopter.net>
2017-04-02 15:21:17 +0200

    Fix test for package

--- tests.py
+++ tests.py
@@ -4,13 +4,13 @@ import tempfile
 
 from flask import url_for
 
-from app import app, db
+from plume.app import app, db
 
 
 class PlumeTest(unittest.TestCase):
     def setUp(self):
-        import models
-        import views # without it, app have no route
+        from plume import models, views
+        # without importing views, app have no route
         app.config["TESTING"] = True
         app.config["SERVER_NAME"] = "localhost"
         self.client = app.test_client()
@@ -25,14 +25,24 @@ class PlumeTest(unittest.TestCase):
     def test_urls(self):
         # url_for require a context
         with app.app_context():
+            # url not ending with a slash redirect
             rv = self.client.get("/page")
             assert 301 == rv.status_code
             assert url_for("page") == rv.location
+            # These page don't need data
             for url in ["page", "page_create"]:
                 rv = self.client.get(url_for(url))
                 assert 200 == rv.status_code, "Error for %s" % url
+            # These page produce 404 without data
             for url in ["page_edit", "page_delete"]:
-                rv = self.client.get(url, data={"pk":1})
+                rv = self.client.get(url_for(url, pk="1"))
+                assert 404 == rv.status_code, "Error for %s" % url
+            # Inserting data in db should fix the issue
+            from plume import models
+            models.Content.create(id=1, title="foo", content="bar", author=1,
+                    slug="foo", status="hd")
+            for url in ["page_edit", "page_delete"]:
+                rv = self.client.get(url_for(url, pk="1"))
                 assert 200 == rv.status_code, "Error for %s" % url
 
     def test_no_data(self):
@@ -40,7 +50,7 @@ class PlumeTest(unittest.TestCase):
         assert b"no pages" in rv.data
 
     def test_save(self):
-        from models import Content
+        from plume.models import Content
         with app.app_context():
             url = url_for("page_create")
         rv = self.client.post(url, data=dict(
